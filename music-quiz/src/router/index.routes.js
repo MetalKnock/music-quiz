d7d0a43c19420a38b441bb050da8a8a7a147a2bd
Dmitry Ivanov <dimannisik1@gmail.com>
2022-11-21 04:33:13 +0700

    fix: add active link for burger menu

--- music-quiz/src/router/index.routes.js
+++ music-quiz/src/router/index.routes.js
@@ -4,17 +4,23 @@ const router = async (route, container) => {
   container.innerHTML = "";
   const header = document.querySelector(".header");
   const headerMenuLinks = header.querySelectorAll(".menu__link");
+  const burgerMenuLinks = header.querySelectorAll(".burger-menu__link");
 
   headerMenuLinks.forEach((headerMenuLink) =>
     headerMenuLink.classList.remove("button--active")
   );
+  burgerMenuLinks.forEach((burgerMenuLink) =>
+    burgerMenuLink.classList.remove("button--active")
+  );
 
   switch (route) {
     case "#/": {
+      burgerMenuLinks[0].classList.add("button--active");
       headerMenuLinks[0].classList.add("button--active");
       return container.appendChild(pages.home());
     }
     case "#/quiz": {
+      burgerMenuLinks[1].classList.add("button--active");
       headerMenuLinks[1].classList.add("button--active");
       return container.appendChild(pages.quiz());
     }
@@ -22,6 +28,7 @@ const router = async (route, container) => {
       return container.appendChild(pages.results());
     }
     case "#/gallery": {
+      burgerMenuLinks[2].classList.add("button--active");
       headerMenuLinks[2].classList.add("button--active");
       return container.appendChild(pages.gallery());
     }
